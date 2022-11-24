885ea9c016017ea9a50039bbba4d4f00b840722f
touhidcodes <touhidcodes@gmail.com>
2022-11-24 14:03:57 +0600

    music api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Fetch Data from Server
+// Fetch Data from Server and Display
 const getData = () => {
   const keyWord = document.getElementById("search").value;
   fetch(`https://api.lyrics.ovh/suggest/${keyWord}`)
@@ -6,10 +6,33 @@ const getData = () => {
     .then((Data) => {
       const data = Data.data;
       fixedData = data.slice(0, 9);
+
+      // Loop Data and Get properties
       for (let i = 0; i < fixedData.length; i++) {
         const musicInfo = fixedData[i];
         const musicTitle = musicInfo.title;
-        console.log(musicTitle);
+        const musicAlbum = musicInfo.title_short;
+        createList(musicTitle, musicAlbum);
+        // console.log(musicTitle, musicAlbum);
       }
+      //console.log(Data);
     });
+};
+const musicList = document.getElementById("new-element");
+musicList.innerHTML = ``;
+
+// Create Element Function
+const createList = (title, album) => {
+  musicList.innerHTML += `
+    <div class="single-result row align-items-center my-3 p-3">
+      <div class="col-md-9">
+        <h3 class="lyrics-name"> ${title} </h3>
+        <p class="author lead">
+          Album by <span> ${album} </span>
+        </p>
+      </div>
+      <div class="col-md-3 text-md-right text-center">
+        <button class="btn btn-success">Get Lyrics</button>
+      </div>
+    </div>`;
 };
