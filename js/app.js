0866810d67d25c8639b79e59f1a76dbd205ba314
touhidcodes <touhidcodes@gmail.com>
2022-11-24 12:31:58 +0600

    music api

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@ const getData = () => {
   const keyWord = document.getElementById("search").value;
   fetch(`https://api.lyrics.ovh/suggest/${keyWord}`)
     .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
+    .then((Data) => {
+      const data = Data.data;
+      fixedData = data.slice(0, 9);
+      for (let i = 0; i < fixedData.length; i++) {
+        const musicInfo = fixedData[i];
+        const musicTitle = musicInfo.title;
+        console.log(musicTitle);
+      }
     });
 };
